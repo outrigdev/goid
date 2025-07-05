88f07724ad3be7c46e8a77bf7094dbd0c6c49eb6
sawka <mike@commandline.dev>
2025-07-04 17:56:25 -0700

    inline hint for non-optimized Get()

--- get_go123_124_fallback.go
+++ get_go123_124_fallback.go
@@ -1,4 +1,4 @@
-//go:build (go1.23 && !go1.25) && !(amd64 || arm64)
+//go:build go1.23 && !go1.25 && !(amd64 || arm64)
 
 // Copyright 2025, Command Line Inc.
 // SPDX-License-Identifier: Apache-2.0
@@ -6,6 +6,8 @@
 package goid
 
 // Get returns the current goroutine ID using stack trace method for Go 1.23-1.24 on non-amd64/arm64 architectures
+//
+//go:inline
 func Get() uint64 {
 	return GetFromStack()
 }
